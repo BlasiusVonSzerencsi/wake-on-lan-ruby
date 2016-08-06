787a6f8dca9d351d90f7ac4564e98a47b52cdfce
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-08-06 19:07:09 +0200

    Assemble WOL frame.

--- lib/wake_on_lan/magic_packet.rb
+++ lib/wake_on_lan/magic_packet.rb
@@ -7,6 +7,14 @@ class WakeOnLan::MagicPacket
   end
 
 
+  def assemble_frame
+    six_times_255 = 0xff.chr * 6
+    binary_mac = mac_address.split(':').reduce('') { |binary, hex| binary << hex.to_i(16).chr }
+
+    six_times_255 + binary_mac * 16
+  end
+
+
   private
 
   def colon_separated(mac_address)
--- spec/wake_on_lan/magic_packet_spec.rb
+++ spec/wake_on_lan/magic_packet_spec.rb
@@ -5,16 +5,39 @@ describe WakeOnLan::MagicPacket do
   describe '#mac_address' do
 
     it 'should return the MAC address' do
-      subject = described_class.new '00:11:22:33:44:55:66:77'
+      subject = described_class.new '00:11:22:33:44:55'
 
-      expect(subject.mac_address).to eq '00:11:22:33:44:55:66:77'
+      expect(subject.mac_address).to eq '00:11:22:33:44:55'
     end
 
 
     it 'should return the MAC address separated by colons' do
-      subject = described_class.new '00-11-22-33-44-55-66-77'
+      subject = described_class.new '00-11-22-33-44-55'
 
-      expect(subject.mac_address).to eq '00:11:22:33:44:55:66:77'
+      expect(subject.mac_address).to eq '00:11:22:33:44:55'
+    end
+
+  end
+
+
+  describe '#assemble_frame' do
+
+    subject { described_class.new '00:11:22:33:44:55' }
+
+    it 'should assemble a frame starting with six times 255' do
+      expect(subject.assemble_frame).to start_with 0xff.chr * 6
+    end
+
+
+    it 'should assemble a frame ending with sixteen repetitions of the MAC address' do
+      bin = [0x00, 0x11, 0x22, 0x33, 0x44, 0x55].map &:chr
+
+      expect(subject.assemble_frame).to end_with bin.join * 16
+    end
+
+
+    it 'should be 102 bytes long' do
+      expect(subject.assemble_frame.bytesize).to eq 102
     end
 
   end
