25598d48ea60b400a3f92c2ede8c7856041846fd
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-08-06 19:24:28 +0200

    Send WOL magic packet via UDP socket.

--- lib/wake_on_lan.rb
+++ lib/wake_on_lan.rb
@@ -1,7 +1,19 @@
-require "wake_on_lan/version"
+require 'socket'
+
+require 'wake_on_lan/version'
 
 module WakeOnLan
 
   autoload :MagicPacket, 'wake_on_lan/magic_packet'
+
+  def self.wake(mac_address, broadcast, port)
+    packet = MagicPacket.new(mac_address).assemble_frame
+
+    socket = UDPSocket.open()
+    socket.setsockopt Socket::SOL_SOCKET, Socket::SO_BROADCAST, 1
+
+    socket.send packet, 0, broadcast, port
+    socket.close
+  end
 
 end
--- spec/wake_on_lan_spec.rb
+++ spec/wake_on_lan_spec.rb
@@ -6,4 +6,22 @@ describe WakeOnLan do
     expect(WakeOnLan::VERSION).not_to be nil
   end
 
+
+  describe '.wake' do
+
+    it 'should send the WOL frame via an UDP socket' do
+      packet = instance_double(WakeOnLan::MagicPacket)
+      allow(WakeOnLan::MagicPacket).to receive(:new).and_return packet
+      allow(packet).to receive(:assemble_frame).and_return 'assembled frame'
+
+      socket = instance_double(UDPSocket).as_null_object
+      allow(UDPSocket).to receive(:open).and_return socket
+
+      expect(socket).to receive(:send).with 'assembled frame', 0, '255.255.255.255', 9
+
+      described_class.wake '00:11:22:33:44:55', '255.255.255.255', 9
+    end
+
+  end
+
 end
